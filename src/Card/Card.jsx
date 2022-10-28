242dc6a655a204bb7c14ef3ca93c635c593b71d7
rezahuda2004 <hudamajor02@gmail.com>
2022-10-28 13:48:30 +0700

    feat: card

---
+++ src/Card/Card.jsx
@@ -0,0 +1,16 @@
+import style from "./card.module.css"
+
+const Card = () => {
+    return (
+        <>
+        <div className={style["coba-2"]}>
+            {/* div classname = {style.card} */}
+        <h2>
+            ini CARD
+        </h2>
+        </div>
+        </>
+    )
+}
+
+export default Card
